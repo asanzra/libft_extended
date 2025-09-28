077a572d3723a59d749fa2f385926ffa31367ac8
asanzra <asanz2003@gmail.com>
2025-09-28 12:34:22 +0100

    Fixed norm (function alignment)

--- lib_extended.h
+++ lib_extended.h
@@ -15,11 +15,11 @@
 # include <stddef.h>
 
 //Get next line export
-char	*get_next_line(int fd);
+char			*get_next_line(int fd);
 
 // Printf exports
-int		ft_printf(const char *format, ...);
-int		ft_printf_color(int color, const char *format, ...);
+int				ft_printf(const char *format, ...);
+int				ft_printf_color(int color, const char *format, ...);
 
 // Basic (libft) exports
 int				ft_isalpha(int c);
@@ -72,14 +72,15 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstdelone(t_list *lst, void (*del)(void *));
-void	ft_lstclear(t_list **lst, void (*del)(void *));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstnew(void *content);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+int				ft_lstsize(t_list *lst);
+t_list			*ft_lstlast(t_list *lst);
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void *));
+void			ft_lstclear(t_list **lst, void (*del)(void *));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
+					void (*del)(void *));
 
 #endif
